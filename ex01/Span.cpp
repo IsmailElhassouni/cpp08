074c4a6dc980ef6fdb1b40446b64e42121810bde
IsmailElhassouni <136853807+IsmailElhassouni@users.noreply.github.com>
2024-11-01 11:41:53 +0000

    need to fic ex01

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -15,20 +15,21 @@ void Span::addNumber(unsigned int n)
     if(check == N)
         throw std::runtime_error("Span is full");
     v.push_back(n);
+    check++;
 }
 unsigned int Span::shortestSpan()
 {
     if (v.size() < 2)
         throw std::runtime_error("Not enough elements");
-    vector<unsigned int> temp = v;
-    std::sort(temp.begin(), temp.end());
-    unsigned int min=temp[temp.size()/2] - temp[temp.size()/2-1];
+    unsigned int min = longestSpan();
+    
     return min;
+
 }
 unsigned int Span::longestSpan()
 {
     if (v.size() < 2)
-        throw std::runtime_error("Not enough elements");
+     throw std::runtime_error("Not enough elements");
     vector<unsigned int> temp = v;
     std::sort(temp.begin(), temp.end());
     unsigned int max=temp[temp.size()-1] - temp[0];
